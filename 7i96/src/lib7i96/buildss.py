221dfbe2ab1e0a1425ba56ce33023d0e4ddfb83a
john <bjt128@gmail.com>
2021-04-29 07:25:56 -0500

    fix building ss hal file

--- 7i96/src/lib7i96/buildss.py
+++ 7i96/src/lib7i96/buildss.py
@@ -13,74 +13,74 @@ def build(parent):
 
 	if parent.ssCardCB.currentText() == '7i64':
 		for i in range(24):
-			if getattr(parent, 'ss7i64in_' + str(i)).text():
+			if getattr(parent, 'ss7i64in_' + str(i)).text() != 'Select':
 				inPin = getattr(parent, 'ss7i64in_' + str(i)).text()
 				contents.append(f'net ss7i64in_{i} hm2_7i96.0.7i64.0.0.input-{i:02} <= {inPin}\n')
 		for i in range(24):
-			if getattr(parent, 'ss7i64out_' + str(i)).text():
+			if getattr(parent, 'ss7i64out_' + str(i)).text() != 'Select':
 				outPin = getattr(parent, 'ss7i64out_' + str(i)).text()
 				contents.append(f'net ss7i64out_{i} hm2_7i96.0.7i64.0.0.output-{i:02} => {outPin}\n')
 
 	elif parent.ssCardCB.currentText() == '7i69':
 		for i in range(24):
-			if getattr(parent, 'ss7i69in_' + str(i)).text():
+			if getattr(parent, 'ss7i69in_' + str(i)).text() != 'Select':
 				inPin = getattr(parent, 'ss7i69in_' + str(i)).text()
 				contents.append(f'net ss7i69in_{i} hm2_7i96.0.7i69.0.0.input-{i:02} <= {inPin}\n')
 		for i in range(24):
-			if getattr(parent, 'ss7i69out_' + str(i)).text():
+			if getattr(parent, 'ss7i69out_' + str(i)).text() != 'Select':
 				outPin = getattr(parent, 'ss7i69out_' + str(i)).text()
 				contents.append(f'net ss7i69out_{i} hm2_7i96.0.7i69.0.0.output-{i:02} => {outPin}\n')
 
 	elif parent.ssCardCB.currentText() == '7i70':
 		for i in range(48):
-			if getattr(parent, 'ss7i70in_' + str(i)).text():
+			if getattr(parent, 'ss7i70in_' + str(i)).text() != 'Select':
 				inPin = getattr(parent, 'ss7i70in_' + str(i)).text()
 				contents.append(f'net ss7i70in_{i} hm2_7i96.0.7i70.0.0.input-{i:02} <= {inPin}\n')
 
 	elif parent.ssCardCB.currentText() == '7i71':
 		for i in range(48):
-			if getattr(parent, 'ss7i71out_' + str(i)).text():
+			if getattr(parent, 'ss7i71out_' + str(i)).text() != 'Select':
 				inPin = getattr(parent, 'ss7i71out_' + str(i)).text()
 				contents.append(f'net ss7i71out_{i} hm2_7i96.0.7i71.0.0.output-{i:02} <= {inPin}\n')
 
 	elif parent.ssCardCB.currentText() == '7i72':
 		for i in range(48):
-			if getattr(parent, 'ss7i72out_' + str(i)).text():
+			if getattr(parent, 'ss7i72out_' + str(i)).text() != 'Select':
 				inPin = getattr(parent, 'ss7i72out_' + str(i)).text()
 				contents.append(f'net ss7i72out_{i} hm2_7i96.0.7i72.0.0.output-{i:02} <= {inPin}\n')
 
 	elif parent.ssCardCB.currentText() == '7i73':
 		for i in range(16):
-			if getattr(parent, 'ss7i73key_' + str(i)).text():
+			if getattr(parent, 'ss7i73key_' + str(i)).text() != 'Select':
 				keyPin = getattr(parent, 'ss7i73key_' + str(i)).text()
 				contents.append(f'net ss7i73key_{i} hm2_7i96.0.7i73.0.0.input-{i:02} <= {keyPin}\n')
 		for i in range(12):
-			if getattr(parent, 'ss7i73lcd_' + str(i)).text():
+			if getattr(parent, 'ss7i73lcd_' + str(i)).text() != 'Select':
 				lcdPin = getattr(parent, 'ss7i73lcd_' + str(i)).text()
 				contents.append(f'net ss7i73lcd_{i} hm2_7i96.0.7i73.0.0.output-{i:02} => {lcdPin}\n')
 		for i in range(16):
-			if getattr(parent, 'ss7i73in_' + str(i)).text():
+			if getattr(parent, 'ss7i73in_' + str(i)).text() != 'Select':
 				inPin = getattr(parent, 'ss7i73in_' + str(i)).text()
 				contents.append(f'net ss7i73in_{i} hm2_7i96.0.7i73.0.0.input-{i:02} <= {inPin}\n')
 		for i in range(2):
-			if getattr(parent, 'ss7i73out_' + str(i)).text():
+			if getattr(parent, 'ss7i73out_' + str(i)).text() != 'Select':
 				outPin = getattr(parent, 'ss7i73out_' + str(i)).text()
 				contents.append(f'net ss7i73out_{i} hm2_7i96.0.7i84.0.0.output-{i:02} => {outPin}\n')
 
 
 	elif parent.ssCardCB.currentText() == '7i84':
 		for i in range(32):
-			if getattr(parent, 'ss7i84in_' + str(i)).text():
+			if getattr(parent, 'ss7i84in_' + str(i)).text() != 'Select':
 				inPin = getattr(parent, 'ss7i84in_' + str(i)).text()
 				contents.append(f'net ss7i84in_{i} hm2_7i96.0.7i84.0.0.input-{i:02} <= {inPin}\n')
 		for i in range(16):
-			if getattr(parent, 'ss7i84out_' + str(i)).text():
+			if getattr(parent, 'ss7i84out_' + str(i)).text() != 'Select':
 				outPin = getattr(parent, 'ss7i84out_' + str(i)).text()
 				contents.append(f'net ss7i84out_{i} hm2_7i96.0.7i84.0.0.output-{i:02} => {outPin}\n')
 
 	elif parent.ssCardCB.currentText() == '7i87':
 		for i in range(32):
-			if getattr(parent, 'ss7i87in_' + str(i)).text():
+			if getattr(parent, 'ss7i87in_' + str(i)).text() != 'Select':
 				inPin = getattr(parent, 'ss7i87in_' + str(i)).text()
 				contents.append(f'net ss7i87in_{i} hm2_7i96.0.7i87.0.0.input-{i:02} <= {inPin}\n')
 
